df08c7db4c39053dcf83fc01f23c97c3bbeb484e
Nacho <mendozaignacio9228@gmail.com>
2022-04-08 19:09:38 -0300

    Section-Information

--- src/components/Card-section/Card.js
+++ src/components/Card-section/Card.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import './Card.css'
 
-export default function card() {
+export default function Card() {
   return (
 <section className="menu" id="menu">
 
 <h1 className="heading"><span>Menu</span> </h1>
 
-    <div className="box-container">
+    <div className="grid grid-cols-3 gap-1 auto-rows-[minmax(20rem,_1fr)] box-container">
 
-        <div className="box">
-            <img src={'https://w7.pngwing.com/pngs/721/224/png-transparent-coffee-cup-breakfast-cafe-coffee-tea-coffee-shop-happy-birthday-vector-images-thumbnail.png'} alt="Menu-1"/>
+        <div className="p-20 text-center box">
+            <img src={'https://scontent.faep15-1.fna.fbcdn.net/v/t39.30808-6/277164234_7588019724571301_2820656686782256509_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_eui2=AeHz9gdEMVrzVnb3ADsw6euEAITS7bNXnWgAhNLts1edaCvP9yPExFt5ZDyebBPU9USZLkjQIz1--ZGrRO97LXJ9&_nc_ohc=xCPiT3uCuzEAX9mcqpU&_nc_ht=scontent.faep15-1.fna&oh=00_AT-LTWcXNgeZGxhfCF2ez43DpEH5dHK-SBhW1GmPl1fzgg&oe=624E5CCB'} alt="Menu-1"/>
             <h3>Café Espresso</h3>
             <div className="price">$00.00 <span>00.00</span></div>
             <a href="#" className="btn">Comprar</a>
         </div>
+
     </div>    
 </section>
   )
--- src/components/Information-section/Information.js
+++ src/components/Information-section/Information.js
@@ -5,48 +5,57 @@ export default function information() {
   return (
 <section className="information" id="#">
     <h1 className="heading"><span>Información</span> </h1>
-    <div className="box-container">
-        <img src={''} alt="Full Coffee"/>
+    <div className="grid grid-cols-6 gap-1 auto-rows-[minmax(20rem,_1fr)] box-container">
+        <img src={'https://pbs.twimg.com/profile_images/1505885583931068419/cWb-cLJD_400x400.jpg'} className='h-48 rounded-full' alt="Full Coffee"/>
             <ul>
-                <h3>Occasions</h3>
-                <li>Occasions</li>
-                <li>Working From Home</li>
-                <li>Snack Time</li>
-                <li>Breakfast</li>
-                <li>Vegan Life</li>
-                <li>Baking</li>
-                <li>Girts n' Bundles</li>
-                <li>Easy Cooking</li>
-                <li>All Products</li>
+                <h3>Tipos de Cafe</h3>
+                <li>Latte</li>
+                <li>Espresso</li>
+                <li>Mocca</li>
+                <li>Americano</li>
+                <li>Bonbom</li>
+                <li>Cappuccino</li>
+                <li>Cafe Negro</li>
+                <li>Frappe</li>
+                <li>Y más</li>
             </ul>
             <ul>
-                <h3>All Categories</h3>
-                <li>Baking</li>
-                <li>Breakfast</li>
-                <li>Condiments</li>
-                <li>Drinks</li>
-                <li>Heat Things Up</li>
-                <li>Meal Solutions</li>
-                <li>Pantry Staples</li>
-                <li>Simmer Sauces</li>
-                <li>Snacks</li>
-                <li>Spice Up Your Cooking</li>
-                <li>Sweet tooth</li>
-                <li>Wine n' Cheese</li>
+                <h3>Nuestras Categorias</h3>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>Menu</li>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Whatsapp</li>
             </ul>
             <ul>
-                <h3>Gifts n' Bundles</h3>
-                <li>Sweet tooth Special</li>
-                <li>Discoverd Local</li>
-                <li>Baking Box</li>
-                <li>Spicy Heat Lover's Box</li>
-                <li>Sensations</li>
+                <h3>De interes</h3>
+                <li>Plantas de Cafe</li>
+                <li>Principales Productores</li>
+                <li>Medio Oriente</li>
+                <li>Vietnam</li>
+                <li>Etiopia</li>
+                <li>Indonesia</li>
+                <li>India</li>
+                <li>Colombia</li>
+                <li>Medio Occidental</li>
+                <li>Brasil</li>
+                <li>Colombia</li>
+                <li>Peru</li>
+                <li>Nicaragua</li>
             </ul>
             <ul>
-                <h3>Special</h3>
+                <h3>Especiales</h3>
+                <li>Crema</li>
+                <li>Chocolate</li>
+                <li>Canela</li>
+                <li>Leche</li>
+                <li>Caramelo</li>
             </ul>
             <ul>
-                <h3>News</h3>
+                <h3>Noticias</h3>
+                <li>Proximamente...</li>
             </ul>
     </div>
 
