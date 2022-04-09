d2dc3bd6c897bb3dd2bbbab98fda8a09d6ba9834
FacundoGPayer <facundo@garciapayer.com>
2022-04-09 00:58:39 -0300

    Eliminando import de Relleno

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Banner from './banner/Banner';
 import Header from './header/Header';
-import Relleno from './Relleno';
 function App() {
   return (
     <div className="App">
