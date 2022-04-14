c7268f57d659377591e87bb00eeff584f9a97aa2
FacundoGPayer <facundo@garciapayer.com>
2022-04-14 18:29:37 -0300

    Separacion de estilos entre index.css y header.css (estilos globales y especificos a header)

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,16 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Banner.css'
 
 export default function Banner() {
-    return (
-            <section className="banner text-center text-white bg-purple-800  flex-wrap justify-center sm:ease-in-out sm:delay-80" id="banner">
+  const [banner,setBanner]= useState(true)
+    return (<>
+            { banner && <section className="banner text-center text-white bg-purple-800  flex-wrap justify-center sm:ease-in-out sm:delay-80" id="banner">
                 <span>50% de descuento en tu segunda unidad</span>
-                <button type='button' className="ml-2 hover:text-blue-500" onClick={() => {var x = document.getElementById("banner");
-                if (x.style.display === "none") {
-                  x.style.display = "block";
-                } else {
-                  x.style.display = "none";
-                }} }>[×]</button>
-            </section>
+                <button type='button' className="ml-2 hover:text-blue-500" onClick={()=> setBanner(false)}>[×]</button>
+            </section>}
+            </>
     )
 }
