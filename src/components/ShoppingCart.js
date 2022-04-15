6050978b1184b2ae6f9acaa210bce04d3a4c39d8
Nacho <mendozaignacio9228@gmail.com>
2022-04-14 23:23:12 -0300

    Carrito

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,41 +2,58 @@
 import { shoppingReducer, shoppingInitialState } from "../components/reducer/shoppingReducer";
 import { TYPES } from "./actions/shoppingActions";
 import { useReducer } from "react";
+import CartItem from "./CartItem";
 import Product from "./Product";
+
 
 const ShoppingCart = () => {
 
     const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState)
 
-    const {product, cart} = state
+    const {products, cart} = state
 
-    const addToCart = () => {}
+    const addToCart = (id) => {
+        dispatch({type: TYPES.ADD_TO_CART, payload: id})
+    }
 
-    const deleteFromCart = () => {}
+    const deleteFromCart = (id, all = false) => {
+        if (all) {
+            dispatch({type: TYPES.REMOVE_ALL_PRODUCTS, payload: id})
+        } else {
+            dispatch({type: TYPES.REMOVE_ONE_PRODUCT, payload: id})
+        }
+  
+    }
 
-    const clearCart = () => {}
+    const clearCart = () => {
+        dispatch({types: TYPES.CLEAR_CART})
+    }
 
 return (
     <>
-    <h2>Carrito de Compras</h2>
-    <h3>Productos</h3>
-        <div className="box grid-responsive">
+        <div className='box-container mb-8 bg-no-repeat bg-cover bg-[url(https://p4.wallpaperbetter.com/wallpaper/363/368/20/dark-wood-texture-wallpaper-preview.jpg)]'>
+    
             {products.map(product => <Product 
-            
-            key={product.id}
-            data={product}
-            addToCart={addToCart}
-            
-            />)}
+                key={product.id}
+                data={product}
+                addToCart={addToCart}
+                />)} 
         </div>
-        <h3>Carrito</h3>
-    <div className="box">
+        <section className="border-2 border-orange-100 border-solid">
+        <h1 className="heading"><span className="text-white">Carrito</span> del Cafe</h1>
+        <div className="bg-white box-container">
+            
+            {cart.map((item, index) => <CartItem 
+            key={index}
+            data={item} deleteFromCart={deleteFromCart}/>
+            )}
 
-    {cart.map ((item, index )=> <CartItem key={index}
-    data={item} deleteFromCart={deleteFromCart} />)}
+        </div>
+        <div className="flex-1 text-center">
+            <button onClick={() => clearCart()} className="text-center mt-4 py-4 px-6 text-2xl text-white rounded-3xl bg-[#ddb17c] hover:text-black">Limpiar Carrito</button>
+        </div>
+        </section>
 
-    </div>
-    <button>Limpiar Carrito</button>
     </>
     );
 };
